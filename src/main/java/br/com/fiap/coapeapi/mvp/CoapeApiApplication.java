5d047c75c18dbabeda6f065bb32cd052c1cffa3c
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2022-04-09 08:58:51 -0300

    first commit

---
+++ src/main/java/br/com/fiap/coapeapi/mvp/CoapeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.coapeapi.mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoapeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoapeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/coapeapi/mvp/CoapeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.coapeapi.mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoapeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
